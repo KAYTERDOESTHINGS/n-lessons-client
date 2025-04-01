1def3a55f57878ed476873a38c319f2732c8f97e
trong.kd <kienductrong@gmail.com>
2025-04-01 21:51:14 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import { useGlobalContext } from "./components/config/GlobalProvider";
 import { PAGE_CONFIG } from "./components/config/PageConfig";
-import { useEffect } from "react";
-import { setStorageData } from "./services/storages";
-import { LOCAL_STORAGE } from "./services/constant";
 import RedirectHome from "./components/config/RedirectHome";
+import LessonClient from "./pages/LessonClient";
 
 const App = () => {
-  const { apiKey, setApiKey } = useGlobalContext();
-
-  useEffect(() => {
-    setStorageData(
-      LOCAL_STORAGE.API_KEY,
-      "MiT71gV0fqbQmrhuSD5nWezgxwJe99tUBfwk9GNfM28cxyKyGy2ZwkjrDZx4iQRghVe1r6yHOsDAvfWpiYnNGJxdudFE7rqSYHIL9mrdQs8SRwKIljV1tzJmRXeovxxS"
-    );
-    setApiKey(
-      "MiT71gV0fqbQmrhuSD5nWezgxwJe99tUBfwk9GNfM28cxyKyGy2ZwkjrDZx4iQRghVe1r6yHOsDAvfWpiYnNGJxdudFE7rqSYHIL9mrdQs8SRwKIljV1tzJmRXeovxxS"
-    );
-  }, []);
+  const { apiKey } = useGlobalContext();
 
   const PAGE_CONFIG_FILTERED = Object.values(PAGE_CONFIG).filter(
     (item: any) => item.path && item.element
@@ -35,7 +23,9 @@ const App = () => {
             ))}
           </>
         ) : (
-          <></>
+          <>
+            <Route path="/" element={<LessonClient />} />
+          </>
         )}
         <Route path="*" element={<NotFound />} />
       </Routes>
